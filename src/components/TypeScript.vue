37b2d6fa466d042690874fb4fd53ba18784f8e07
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-06-05 19:58:34 +0200

    Composants Vue utilisant TypeScript

---
+++ src/components/TypeScript.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const name: Ref<string> = ref('unknow');
+const age: Ref<number> = ref(28);
+
+function changeName(newName: string) {
+  name.value = newName
+}
+function changeAge(newName: number) {
+  age.value = newName
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="changeName('Codwerk')">change Name {{ name }}</button>
+    <button type="button" @click="changeAge(29)">change Name {{ age }}</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
